037be40c477e0324fea8872d6721242fa1ac1f29
Malcom King <kingmalcom@rocketmail.com>
2018-09-22 19:30:22 -0500

    FInish web caesar

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
-from flask import Flask 
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-add_form = """
+form = """
 <!DOCtYPE html>
 
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px san-serif;
                 border-readius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method ='POST'>
-            <label>Key
-                <input name="rot" type="text' value='{0}' />
+            <label>Rotate:
+                <input name="rot" type="text' value='0'/>
             </label>
-
-            <label>Message
-                <textarea name="text"></ textarea>
-            </label>
+            <input type="submit"/>
+            
+                <textarea name="text" type="text">{0}</textarea>
+            
+            <input type="submit" value="Submit"/>
         </form>
     </body>
 </html>
@@ -40,6 +42,18 @@ add_form = """
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return form.format("Enter Message")
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    
+    rot = request.form['rot']
+    text = request.form['text']
+    rot = int(rot)
+    
+    new_text = rotate_string(text, rot)
+    
+    return form.format(new_text)
+
 
 app.run()    
